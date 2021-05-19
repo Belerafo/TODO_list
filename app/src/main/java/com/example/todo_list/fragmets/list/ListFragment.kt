6fd0f2d8b262d763e7d71869463ad186d1b4657f
Belerafo <68774802+Belerafo@users.noreply.github.com>
2021-05-19 16:15:01 +0300

    1

--- app/src/main/java/com/example/todo_list/fragmets/list/ListFragment.kt
+++ app/src/main/java/com/example/todo_list/fragmets/list/ListFragment.kt
@@ -1,10 +1,8 @@
 package com.example.todo_list.fragmets.list
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.todo_list.R
 import kotlinx.android.synthetic.main.fragment_list.view.*
@@ -25,8 +23,21 @@ class ListFragment : Fragment() {
 
         }
 
+        view.listLayout.setOnClickListener{
+
+            findNavController().navigate(R.id.action_listFragment_to_updateFragment)
+
+        }
+
+        //Set Menu
+        setHasOptionsMenu(true)
+
         return view
     }
 
-   
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.list_fragment_menu, menu)
+    }
+
+
 }
